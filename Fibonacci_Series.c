7dce4f18a773581dabfaffb3758c3000d364eda8
Swarnendu009 <swarnendughosh2003@gmail.com>
2023-10-05 16:24:53 +0530

    PROGRAMS

---
+++ Fibonacci_Series.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int limit, temp1=1, temp2=1, backup;
+    printf("ENTER THE LIMIT: \n");
+    scanf("%d",&limit);
+    for(int i=0;i<=limit;i++)
+    {
+        for(int j=0;j<temp1;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+        backup=temp2;
+        temp2=temp1+temp2;
+        temp1=backup;
+    }
+}
